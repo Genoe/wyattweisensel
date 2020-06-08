4dec8b365af249d80eafb1837033347e04a786e4
Wyatt Weisensel <wweisensel@gmail.com>
2020-06-07 22:32:21 -0500

    Created Navbar with github and resume links

---
+++ src/containers/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div class="nav-wrapper container">
+                <Link to='/'>Wyatt Weisensel</Link>
+                <ul class="right">
+                    <li><Link to='/resume'>Resume</Link></li>
+                    <li><a href="https://github.com/Genoe">My GitHub</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
