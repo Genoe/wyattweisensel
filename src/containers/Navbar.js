74dfb888deca912bc9ee3f1f94bd912cca8b49e8
Wyatt Weisensel <wweisensel@gmail.com>
2020-07-26 21:14:50 -0500

    Added slide-out menu for mobile

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {Link} from 'react-router-dom';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 export default function Navbar() {
+    // const sidenav = useRef(null);
+    useEffect(() => {
+        let sidenav = document.querySelector('#slide-out');
+        M.Sidenav.init(sidenav, {});
+    });
     return (
-        <nav>
-            <div className="nav-wrapper container">
-                <Link to='/' className='brand-logo'>Wyatt Weisensel</Link>
-                <ul className="right">
-                    <li><Link to='/resume'>Resume</Link></li>
-                    <li><a href="https://github.com/Genoe">My GitHub</a></li>
-                </ul>
-            </div>
-        </nav>
+        <React.Fragment>
+            <nav>
+                <div className="nav-wrapper container">
+                    <Link to='/' className='brand-logo'>Wyatt Weisensel</Link>
+                    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><Link to='/resume'>Resume</Link></li>
+                        <li><a href="https://github.com/Genoe">My GitHub</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <ul class="sidenav" id="slide-out">
+                <li><Link to='/resume'>Resume</Link></li>
+                <li><a href="https://github.com/Genoe">My GitHub</a></li>
+            </ul>
+        </React.Fragment>
     );
 }
